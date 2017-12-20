8669956f0d134efac074af76f95c4332e1b658b9
pospolitaki <pospolitaki.agmail.com>
2017-12-20 15:04:50 +0200

    email handling, forms, views

---
+++ newsletter/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Join
+
+
+class JoinForm(forms.ModelForm):
+    email = forms.EmailField(label='', widget=forms.EmailInput(attrs={'placeholder':'Your Email', 'class':'form-control'})) #overrides fields =['email']
+    class Meta:
+        model = Join
+        fields = [
+        'email'            
+        ]
+    
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email') 
+        print(email)
+        qs = Join.objects.filter(email__iexact=email)
+        if qs.exists():
+            print('exists')
+            raise forms.ValidationError('This email already exists')
+        return email
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, FormView, CreateView
+from newsletter.forms import JoinForm 
+# class HomeView(View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, 'pages/home.html', {})
 
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'pages/home.html', {})
 
-        
+class HomeView(CreateView):
+    template_name   = 'pages/home.html'
+    form_class      = JoinForm
+    success_url     = '/'
+
+    # def form_clean(self, form):
+    #     email = form.cleaned_data.get('email')
+    #     return super().form_valid(form) #CreateView - will do all this stuff from this function for us, will create an item in database..
